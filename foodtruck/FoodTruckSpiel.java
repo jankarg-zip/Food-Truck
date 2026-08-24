5350431feb4fd09034120665e58f76c9a43a57ed
jankarg-zip <jan.karg@edu.tbz.ch>
2026-08-24 08:55:46 +0200

    Home Screen

---
+++ foodtruck/FoodTruckSpiel.java
@@ -0,0 +1,23 @@
+package foodtruck;
+
+import java.util.Locale;
+
+public class FoodTruckSpiel {
+    public static void main(String[] args) {
+        System.out.println("+======================================+");
+        System.out.println("|   FFFFF  OOOOO  OOOOO  DDDD         |");
+        System.out.println("|   F      O   O  O   O  D   D        |");
+        System.out.println("|   FFFF   O   O  O   O  D   D        |");
+        System.out.println("|   F      O   O  O   O  DDDD         |");
+        System.out.println("|                                      |");
+        System.out.println("|   TTTTT  RRRR   U   U  CCCC  K  K   |");
+        System.out.println("|     T    R   R  U   U  C     K K    |");
+        System.out.println("|     T    RRRR   U   U  C     KK     |");
+        System.out.println("|     T    R  R    UUU   CCCC  K  K   |");
+        System.out.println("+======================================+");
+        System.out.println();
+        System.out.println("Willkommen bei Food Truck!".toUpperCase(Locale.GERMAN));
+        System.out.println("Dein Abenteuer auf vier Raedern beginnt.");
+        System.out.println("Bereite leckeres Essen zu und werde zum Food-Truck-Star!");
+    }
+}
