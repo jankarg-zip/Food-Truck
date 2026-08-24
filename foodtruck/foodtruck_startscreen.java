0273c329b99895afa4c0233010b6a960e384a9b4
jankarg-zip <jan.karg@edu.tbz.ch>
2026-08-24 09:57:48 +0200

    test

--- foodtruck/foodtruck_startscreen.java
+++ foodtruck/foodtruck_startscreen.java
@@ -18,6 +18,7 @@ public class foodtruck_startscreen {
         System.out.println();
         System.out.println("Willkommen bei Food Truck!".toUpperCase(Locale.GERMAN));
         System.out.println("Dein Abenteuer auf vier Raedern beginnt!.");
+        System.out.println("test.");
         System.out.println("Bereite leckeres Essen zu und werde zum Food-Truck-Star!");
     }
 }
