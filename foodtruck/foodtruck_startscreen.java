73a2289908bdee28136ed9cb77cfdb81ff966799
jankarg-zip <jan.karg@edu.tbz.ch>
2026-08-24 09:17:02 +0200

    Update

--- foodtruck/foodtruck_startscreen.java
+++ foodtruck/foodtruck_startscreen.java
@@ -8,8 +8,8 @@ public class foodtruck_startscreen {
         System.out.println("|   FFFFF  OOOOO  OOOOO  DDDD         |");
         System.out.println("|   F      O   O  O   O  D   D        |");
         System.out.println("|   FFFF   O   O  O   O  D   D        |");
-        System.out.println("|   F      O   O  O   O  DDDD         |");
-        System.out.println("|                                      |");
+        System.out.println("|   F      O 0 O  O 0 O  DDDD         |");
+        System.out.println("|                                     |");
         System.out.println("|   TTTTT  RRRR   U   U  CCCC  K  K   |");
         System.out.println("|     T    R   R  U   U  C     K K    |");
         System.out.println("|     T    RRRR   U   U  C     KK     |");
